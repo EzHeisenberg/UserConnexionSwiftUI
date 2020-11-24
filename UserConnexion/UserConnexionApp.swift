2d9de9be83cf70c25977aecbd474eb09fdfe7c29
Lucas Reteau <lucaporal@live.fr>
2020-11-24 17:12:10 +0100

    Initial Commit

---
+++ UserConnexion/UserConnexionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserConnexionApp.swift
+//  UserConnexion
+//
+//  Created by Ez Heisenberg on 24/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct UserConnexionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
